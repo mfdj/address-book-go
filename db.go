8bffa4a9d0f56bb8762707b2a51ee987983db44c
Mark Fox <mf@ixel.org>
2017-09-25 20:09:44 -0700

    basic fetch of address

--- db.go
+++ db.go
@@ -6,16 +6,42 @@ import (
    "fmt"
 )
 
+var db *sql.DB
+
+type Address struct {
+   PersonId int
+   Street   string
+   City     string
+   State    string
+   Zip      string
+}
+
 func main() {
    db, err := sql.Open("mysql", "goldie:gopher@/address_book_go")
    defer db.Close()
 
    if err != nil {
-      panic(err.Error())
+       panic(err.Error())
    }
 
    err = db.Ping()
    if err != nil {
-       panic(err.Error())
+      panic(err.Error())
    }
+
+   rows, err := db.Query("SELECT person_id, street, city, state, zip FROM address")
+   if err != nil {
+      return
+   }
+   defer rows.Close()
+   
+   for rows.Next() {
+      adrs := new(Address)
+      err := rows.Scan(&adrs.PersonId, &adrs.Street, &adrs.City, &adrs.State, &adrs.Zip)
+      if err != nil {
+         panic(err.Error())
+         return
+      }
+      fmt.Println(adrs)
+    }
 }
